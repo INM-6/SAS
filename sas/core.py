ec31925f812d2708854e5c4d6aac6c1e0ae002e2
jasperalbers <jasperalbers@posteo.de>
2024-04-22 10:23:06 +0200

    remove SAS-VEC
    Co-authored-by: ackurth <anno.kurth@posteo.de>

--- sas/core.py
+++ sas/core.py
@@ -48,67 +48,21 @@ def compare(matrix_a, matrix_b, method='match_values'):
         U_a = U_a[:, :dim_1]
         U_b = U_b[:, :dim_1]
 
-    if method == 'match_values':
+    angles_noflip = (angle(U_a, U_b, method='columns') + angle(V_at, V_bt, method='rows')) / 2
+    angles_flip = np.pi - angles_noflip
+    angles = np.minimum(angles_noflip, angles_flip)
+    weights = (S_a + S_b) / 2
 
-        angles_noflip = (angle(U_a, U_b, method='columns') + angle(V_at, V_bt, method='rows')) / 2
-        angles_flip = np.pi - angles_noflip
-        angles = np.minimum(angles_noflip, angles_flip)
-        weights = (S_a + S_b) / 2
+    # if one singular vector projects to 0, discard it
+    zero_mask = (S_a > np.finfo(float).eps) | (S_b > np.finfo(float).eps)
+    weights = weights[zero_mask]
+    angles = angles[zero_mask]
 
-        # if one singular vector projects to 0, discard it
-        zero_mask = (S_a > np.finfo(float).eps) | (S_b > np.finfo(float).eps)
-        weights = weights[zero_mask]
-        angles = angles[zero_mask]
-
-        weights /= np.sum(weights)
-        smallness = 1 - angles / (np.pi / 2)
-        weighted_smallness = smallness * weights
-        similarity_score = np.sum(weighted_smallness)
-        return similarity_score
-
-    elif method == 'match_vectors':
-
-        product_U = U_a.T @ U_b
-        product_V = V_at @ V_bt.T
-
-        # catch floats that are slightly above 1 or below -1 due to machine precision
-        product_U[np.isclose(product_U, 1)] = 1
-        product_V[np.isclose(product_V, 1)] = 1
-        product_U[np.isclose(product_U, -1)] = -1
-        product_V[np.isclose(product_V, -1)] = -1
-
-        angles_U = np.arccos(product_U)
-        angles_V = np.arccos(product_V)
-
-        angles_noflip = (angles_U + angles_V) / 2
-        angles_flip = np.pi - angles_noflip
-
-        angles = np.minimum(angles_noflip, angles_flip)
-
-        weights = np.zeros((angles.shape[0]))
-        smallness = np.zeros((angles.shape[0]))
-        for row in range(angles.shape[0]):
-
-            # calculate matching vectors
-            minimal_angle = np.min(angles)
-            index_0, index_1 = np.where(angles == minimal_angle)
-
-            # if multiple pairings exist with the same minimal value, pick random one
-            if (len(index_0) != 1):
-                index = np.random.choice(np.arange(len(index_0)))
-                index_0 = index_0[index]
-                index_1 = index_1[index]
-
-            weights[row] = np.sqrt(S_a[index_0] * S_b[index_1])
-            angles = np.delete(angles, index_0, axis=0)
-            angles = np.delete(angles, index_1, axis=1)
-
-            smallness[row] = 1 - minimal_angle / (np.pi / 2)
-
-        return np.sum(weights * smallness) / np.sum(weights)
-
-    else:
-        raise ValueError('Undefined method. Please choose either match_values or match_vectors')
+    weights /= np.sum(weights)
+    smallness = 1 - angles / (np.pi / 2)
+    weighted_smallness = smallness * weights
+    similarity_score = np.sum(weighted_smallness)
+    return similarity_score
 
 
 def effect_size(dist_a, dist_b):
